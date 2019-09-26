0f5e0990223e13ebbc0731f18a1cdd77b76529de
catherning <39702739+catherning@users.noreply.github.com>
2019-09-26 10:35:06 +0800

    Clean structure of repository

---
+++ recipe_1m_analysis/utils.py
@@ -0,0 +1,31 @@
+class Vocabulary(object):
+    """Simple vocabulary wrapper."""
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+
+    def add_word(self, word, idx=None):
+        if idx is None:
+            if not word in self.word2idx:
+                self.word2idx[word] = self.idx
+                self.idx2word[self.idx] = word
+                self.idx += 1
+            return self.idx
+        else:
+            if not word in self.word2idx:
+                self.word2idx[word] = idx
+                if idx in self.idx2word.keys():
+                    self.idx2word[idx].append(word)
+                else:
+                    self.idx2word[idx] = [word]
+
+                return idx
+
+    def __call__(self, word):
+        if not word in self.word2idx:
+            return self.word2idx['<pad>']
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.idx2word)
