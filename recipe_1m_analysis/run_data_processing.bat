9825d8c09ea534c302b23a1f4b31c71241ad3ad2
catherning <39702739+catherning@users.noreply.github.com>
2019-10-06 16:53:33 +0800

    Clean folder and utils file

---
+++ recipe_1m_analysis/run_data_processing.bat
@@ -0,0 +1,9 @@
+python "D:\Documents\food_recipe_gen\recipe_1m_analysis\data_processing.py" ^
+ --recipe1m_path "D:\Google Drive\Catherning Folder\THU\Thesis\Recipe datasets\Recipe1M\recipe1M_layers" ^
+ --save_path "D:\Documents\food_recipe_gen\recipe_1m_analysis\data\normalized" ^
+--threshold_ingrs 50 ^
+--minnuminstrs 9 ^
+--maxnuminstr 11 ^
+--maxnumingrs 11 ^
+--minnumingrs 9 ^
+--minnumwords 50
--- recipe_1m_analysis/utils.py
+++ recipe_1m_analysis/utils.py
@@ -3,15 +3,17 @@ import pickle
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-FOLDER_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),"data")
+FOLDER_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
 DATA_FILES = ["allingrs_count.pkl",
-                "allwords_count.pkl",
-                "recipe1m_test.pkl",
-                "recipe1m_vocab_ingrs.pkl",
-                "recipe1m_vocab_toks.pkl"]
+              "allwords_count.pkl",
+              "recipe1m_test.pkl",
+              "recipe1m_vocab_ingrs.pkl",
+              "recipe1m_vocab_toks.pkl"]
+
 
 class Vocabulary(object):
     """Simple vocabulary wrapper."""
+
     def __init__(self):
         self.word2idx = {}
         self.idx2word = {}
@@ -19,13 +21,13 @@ class Vocabulary(object):
 
     def add_word(self, word, idx=None):
         if idx is None:
-            if not word in self.word2idx:
+            if word not in self.word2idx:
                 self.word2idx[word] = self.idx
                 self.idx2word[self.idx] = word
                 self.idx += 1
             return self.idx
         else:
-            if not word in self.word2idx:
+            if word not in self.word2idx:
                 self.word2idx[word] = idx
                 if idx in self.idx2word.keys():
                     self.idx2word[idx].append(word)
@@ -35,13 +37,12 @@ class Vocabulary(object):
                 return idx
 
     def __call__(self, word):
-        if not word in self.word2idx:
+        if word not in self.word2idx:
             return self.word2idx['<pad>']
         return self.word2idx[word]
 
     def __len__(self):
         return len(self.idx2word)
-
 
 
 class RecipesDataset(Dataset):
@@ -52,8 +53,8 @@ class RecipesDataset(Dataset):
         Args:
             file (string): Path to the file
         """
-        with open(file,'rb') as f:
-            self.data=pickle.load(f)
+        with open(file, 'rb') as f:
+            self.data = pickle.load(f)
 
     def __len__(self):
         return len(self.data)
@@ -61,27 +62,27 @@ class RecipesDataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        
-        sample = {'title': self.data[idx]["title"], 
+
+        sample = {'title': self.data[idx]["title"],
                   'ingredients': self.data[idx]["ingredients"],
-                  'instructions':self.data[idx]["tokenized"],}
+                  'instructions': self.data[idx]["tokenized"], }
 
         return sample
 
+
 if __name__ == "__main__":
 
-    recipe_dataset=RecipesDataset(os.path.join(FOLDER_PATH,DATA_FILES[2]))
+    recipe_dataset = RecipesDataset(os.path.join(FOLDER_PATH, DATA_FILES[2]))
     dataset_loader = torch.utils.data.DataLoader(recipe_dataset,
-                                                batch_size=32, shuffle=True,
-                                                num_workers=4)
+                                                 batch_size=32, shuffle=True,
+                                                 num_workers=4)
 
-    with open(os.path.join(FOLDER_PATH,DATA_FILES[3]),'rb') as f:
-        vocab_ingrs=pickle.load(f)
+    with open(os.path.join(FOLDER_PATH, DATA_FILES[3]), 'rb') as f:
+        vocab_ingrs = pickle.load(f)
 
-
-    recipe=recipe_dataset[0]
-    ingr=recipe["ingredients"]
-    ingr_idx=[]
+    recipe = recipe_dataset[0]
+    ingr = recipe["ingredients"]
+    ingr_idx = []
     for el in ingr:
         ingr_idx.append(vocab_ingrs.word2idx[el])
 
