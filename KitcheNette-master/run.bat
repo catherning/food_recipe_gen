601442e6e820c163b93f5bc78e13e2a34b9ed867
catherning <39702739+catherning@users.noreply.github.com>
2019-12-19 17:26:18 +0800

    Fix run.bat. Add __init__ to KitcheNette\tasks

--- KitcheNette-master/run.bat
+++ KitcheNette-master/run.bat
@@ -1,6 +1,6 @@
 
 cd KitCheNette-master
-python main.py
+python main.py ^
                 --save-prediction-unknowns True ^
                 --model-name "kitchenette_pretrained.mdl" ^
                 --unknown-path "./data/main_pairing_prediction.csv" ^
