26c52dc0293ff3a86017e150c00896f2403ce058
catherning <catherning.xu@gmail.com>
2019-12-19 20:14:59 +0800

    Change run.sh

--- KitcheNette-master/run.sh
+++ KitcheNette-master/run.sh
@@ -1,6 +1,4 @@
-
-cd KitCheNette-master
-python main.py \
+python3 ./main.py \
                 --save-prediction-unknowns True \
                 --model-name "kitchenette_pretrained.mdl" \
                 --unknown-path "./data/main_pairing_prediction.csv" \
