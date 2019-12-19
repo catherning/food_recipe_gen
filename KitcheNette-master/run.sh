23c94bb068d3364248a32e6950fe8fad088aca5f
catherning <39702739+catherning@users.noreply.github.com>
2019-12-19 18:08:07 +0800

    Create run.sh

---
+++ KitcheNette-master/run.sh
@@ -0,0 +1,9 @@
+
+cd KitCheNette-master
+python main.py \
+                --save-prediction-unknowns True \
+                --model-name "kitchenette_pretrained.mdl" \
+                --unknown-path "./data/main_pairing_prediction.csv" \
+                --embed-path "./data/kitchenette_embeddings.pkl" \
+                --data-path "./data/kitchenette_dataset.pkl" \
+                --dist-fn "widedeep"
